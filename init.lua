3e249e3df898f81de4b7fdcb5b5b90ba00596d80
idk <hakabol@proton.me>
2026-09-06 13:07:57 +0530

    intallation guide

--- init.lua
+++ init.lua
@@ -1013,9 +1013,9 @@ require('lazy').setup({
 local mark = require 'harpoon.mark'
 local ui = require 'harpoon.ui'
 
-vim.keymap.set('n', '<leader>am', mark.add_file)
-vim.keymap.set('n', '<leader>au', ui.toggle_quick_menu)
-vim.keymap.set('n', '<leader>a1', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<leader>a2', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<leader>a3', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<leader>a4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>mm', mark.add_file)
+vim.keymap.set('n', '<leader>mu', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>m1', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>m2', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>m3', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>m4', function() ui.nav_file(4) end)
---
+++ readme.md
@@ -0,0 +1,42 @@
+## Dependencies
+
+- [tree-sitter-cli](https://tree-sitter.github.io/tree-sitter/)
+
+- [nodejs](https://nodejs.org/)
+
+- nvim (duh)
+
+### Arch Installation
+
+```bash
+sudo pacman -S nodejs treesitter-cli
+```
+
+### Windows Installation
+
+```bash
+npm install -g tree-sitter-cli
+```
+
+### Ubuntu Installation
+
+```bash
+sudo apt install nodejs npm
+npm install -g tree-sitter-cli
+```
+
+## installation
+
+after installing all the dependencies you can procede to the installation
+
+1. go to your config file in linux and mac it is in ~/.config and in windows it is in C:\Users\<your-username>\AppData\Local
+
+2. move your old config somewhere else
+
+3. clone the repo
+
+```bash
+git clone https://github.com/hakabol/nvim.nvim nvim
+```
+
+4. now you are done and can enjoy nvim
